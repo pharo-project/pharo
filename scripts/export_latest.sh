a983897c4926492d1f162da91044f3c85b8a2614
guille <guillermopolito@gmail.com>
2016-07-25 15:10:31 +0200

    added debug messages to export script

--- scripts/export_latest.sh
+++ scripts/export_latest.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
 PWD="$( dirname $(readlink -f $0) )"
+
+echo "GET LATEST PHARO"
 wget --quiet -O - get.pharo.org/60+vm | bash
+VERSION=`./pharo Pharo.image eval SystemVersion current highestUpdate`
+
+echo "GOT ${VERSION}!"
+
+echo "INSTALL GIT FILETREE SUPPORT"
 ./pharo Pharo.image --save ${PWD}/pre_install_exporter.st
 
-$VERSION=`./pharo Pharo.image eval SystemVersion current highestUpdate`
+echo "EXPORTING SOURCE CODE"
+./pharo Pharo.image --save ${PWD}/export.st
+
+echo "GIT COMMIT, TAG, AND PUSH"
 git add *
 git commit -m "EXPORT VERSION ${VERSION}"
 git tag -a v${VERSION} -m "TAG VERSION ${VERSION}"
