f71ef620c02ffaee9df5225de364049c20d230df
guille <guillermopolito@gmail.com>
2016-07-26 14:07:19 +0200

    fixed commit command

--- scripts/export_latest.sh
+++ scripts/export_latest.sh
@@ -16,7 +16,8 @@ rm -rf src
 ./pharo Pharo.image --save --quit ${PWD}/export.st
 
 echo "GIT COMMIT, TAG, AND PUSH"
-git add pharo-core
+git add src
+git add -u
 git commit -m "EXPORT VERSION ${VERSION}"
 git tag -a v${VERSION} -m "TAG VERSION v${VERSION}"
 git push --force --tags git@github.com:guillep/pharo-core.git
