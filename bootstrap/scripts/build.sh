839b7d4e3bb8254187f7a242b6c65dc590b956dc
guille <guillermopolito@gmail.com>
2016-12-21 13:42:17 +0100

    enhanced build script

--- bootstrap/scripts/build.sh
+++ bootstrap/scripts/build.sh
@@ -1,15 +1,30 @@
+#!/usr/bin/bash
+set -e
+
+#Get inside the bootstrap-cache folder. Pharo interprets relatives as relatives to the image and not the 'working directory'
+cd bootstrap-cache
+
+#Prepare
+echo "Prepare Bootstrap files"
+rm *.fuel
+rm PharoDebug.log
+unzip ../resources/fonts/BitmapDejaVuSans.fuel -d .
+
 #Bootstrap Initialization: Class and RPackage initialization
 echo "Bootstrap Initialization: Class and RPackage initialization"
-rm bootstrap.image && cp bootstrap.image.backup bootstrap.image && rm PharoDebug.log
-./pharo bootstrap-cache/bootstrap.image st bootstrap/scripts/01-initialization/init.st --save --quit
-./pharo bootstrap-cache/bootstrap.image st bootstrap/scripts/01-initialization/initRPackageOrganizer.st --save --quit
+../pharo bootstrap.image st ../bootstrap/scripts/01-initialization/01-init.st --save --quit
+../pharo bootstrap.image st ../bootstrap/scripts/01-initialization/02-initRPackageOrganizer.st --save --quit
 
 #Bootstrap Monticello Part 1: Core and Local repositories
 echo "Bootstrap Monticello Part 1: Core and Local repositories"
-./pharo bootstrap-cache/bootstrap.image st bootstrap-cache/st-cache/Monticello.st --save --quit
-./pharo bootstrap-cache/bootstrap.image st bootstrap/scripts/02-monticello-bootstrap/fixLocalMonticello.st --save --quit
-./pharo bootstrap-cache/bootstrap.image st bootstrap/scripts/02-monticello-bootstrap/loadMonticello.st --save --quit
+../pharo bootstrap.image st st-cache/Monticello.st --save --quit
+../pharo bootstrap.image st ../bootstrap/scripts/02-monticello-bootstrap/01-fixLocalMonticello.st --save --quit
+../pharo bootstrap.image st ../bootstrap/scripts/02-monticello-bootstrap/02-bootstrapMonticello.st --save --quit
 
 #Bootstrap Monticello Part 2: Networking Packages and Remote Repositories
 echo "Bootstrap Monticello Part 2: Networking Packages and Remote Repositories"
-./pharo bootstrap-cache/bootstrap.image st bootstrap/scripts/02-monticello-bootstrap/loadMonticelloRemote.st --save --quit
+../pharo bootstrap.image st ../bootstrap/scripts/02-monticello-bootstrap/03-bootstrapMonticelloRemote.st --save --quit
+
+#Bootstrap Metacello
+echo "Bootstrap Metacello"
+../pharo bootstrap.image st ../bootstrap/scripts/03-metacello-bootstrap/01-loadMetacello.st --save --quit
