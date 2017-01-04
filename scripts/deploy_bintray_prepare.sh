d0715e0c454d61137d1be583860f6e2c3d8f5afa
guille <guillermopolito@gmail.com>
2017-01-04 16:09:38 +0100

    fixed permisions and version names in perl script

--- scripts/deploy_bintray_prepare.sh
+++ scripts/deploy_bintray_prepare.sh
@@ -1,4 +1,1 @@
 echo "`cat .bintray.json | scripts/deploy_bintray_prepare_metadata.smudge`" > .bintray.json
-sed -i.bak 's/$Rev: \([0-9][0-9]*\) \$/\1/' .bintray.json
-sed -i.bak 's/$Date: \(.*\) \$/\1/' .bintray.json
-rm -f .bintray.json.bak
