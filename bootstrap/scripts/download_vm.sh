aa0d7e2216631db873a9fbd46eee441f17bc3598
guille <guillermopolito@gmail.com>
2017-01-17 10:37:00 +0100

    downloading sources with VM

--- bootstrap/scripts/download_vm.sh
+++ bootstrap/scripts/download_vm.sh
@@ -5,4 +5,6 @@ cd vm
 echo `pwd`
 wget http://files.pharo.org/vm/pharo-spur${BOOTSTRAP_ARCH}/linux/latest-threaded.zip
 unzip latest-threaded.zip
+wget http://files.pharo.org/sources/PharoV50.sources.zip
+unzip PharoV50.sources.zip
 cd ..
