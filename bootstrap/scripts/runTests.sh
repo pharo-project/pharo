779638950a59afd2f8965bfe9781fec090f0013a
Pablo Tesone <tesonep@gmail.com>
2017-08-03 22:35:49 +0200

    Fixing the architecture.

--- bootstrap/scripts/runTests.sh
+++ bootstrap/scripts/runTests.sh
@@ -11,14 +11,14 @@ if [ ${1} = "64" ]; then
     ARCHFLAG="64/"
 fi
 
-ls -la
+find bootstrap-cache
 
 wget -O- get.pharo.org/${ARCHFLAG}vm70 | bash
 					
-IMAGE_ARCHIVE=`find bootstrap-cache -name 'Pharo7.0-${architecture}bit-*.zip'`
+IMAGE_ARCHIVE=$(find bootstrap-cache -name 'Pharo7.0-${1}bit-*.zip')
 unzip $IMAGE_ARCHIVE
-IMAGE_FILE=`find . -name 'Pharo7.0-${architecture}bit-*.image'`
-CHANGES_FILE=`find . -name 'Pharo7.0-${architecture}bit-*.changes'`
+IMAGE_FILE=`find . -name 'Pharo7.0-${1}bit-*.image'`
+CHANGES_FILE=`find . -name 'Pharo7.0-${1}bit-*.changes'`
 				
 cp bootstrap-cache/*.sources .
 mv $IMAGE_FILE Pharo.image
