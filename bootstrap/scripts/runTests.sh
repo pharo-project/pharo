fc2ace29bb6bd8aa5d32d0aa205dacfffcb7e87d
Guillermo <guillermopolito@gmail.com>
2017-08-03 21:56:59 +0200

    Update runTests.sh

--- bootstrap/scripts/runTests.sh
+++ bootstrap/scripts/runTests.sh
@@ -13,8 +13,7 @@ fi
 
 wget -O- get.pharo.org/${ARCHFLAG}vm70 | bash
 					
-IMAGE_ARCHIVE = `find bootstrap-cache -name 'Pharo7.0-${architecture}bit-*.zip'`
-
+IMAGE_ARCHIVE=`find bootstrap-cache -name 'Pharo7.0-${architecture}bit-*.zip'`
 unzip $IMAGE_ARCHIVE
 IMAGE_FILE=`find . -name 'Pharo7.0-${architecture}bit-*.image'`
 CHANGES_FILE=`find . -name 'Pharo7.0-${architecture}bit-*.changes'`
