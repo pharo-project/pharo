b15fcf3d761acb78a4089ae2cf7d4b3dfafb1360
Guille Polito <guillermopolito@gmail.com>
2017-08-07 13:42:33 +0200

    added 64 bits preparation also

--- bootstrap/scripts/prepare_for_upload.sh
+++ bootstrap/scripts/prepare_for_upload.sh
@@ -1,18 +1,33 @@
 #Get the hash of the built image
 HASH=$(find Pharo7.0-32bit-*.zip | head -n 1 | cut -d '-' -f 3 | cut -d '.' -f 1)
-FULL_IMAGE_NAME="Pharo7.0-32bit-${HASH}.zip"
-MINIMAL_IMAGE_NAME="Pharo7.0-metacello-32bit-${HASH}.zip"
+FULL_IMAGE_NAME32="Pharo7.0-32bit-${HASH}.zip"
+FULL_IMAGE_NAME64="Pharo7.0-64bit-${HASH}.zip"
+
+MINIMAL_IMAGE_NAME32="Pharo7.0-metacello-32bit-${HASH}.zip"
+MINIMAL_IMAGE_NAME64="Pharo7.0-metacello-64bit-${HASH}.zip"
 BUILD_NUMBER=${BUILD_NUMBER:-nobuildnumber}
 
-cp "${FULL_IMAGE_NAME}" latest.zip
-cp "${FULL_IMAGE_NAME}" latest-32.zip
-cp "${MINIMAL_IMAGE_NAME}" latest-minimal.zip
-cp "${MINIMAL_IMAGE_NAME}" latest-minimal-32.zip
+cp "${FULL_IMAGE_NAME32}" latest.zip
+cp "${FULL_IMAGE_NAME32}" latest-32.zip
+cp "${FULL_IMAGE_NAME64}" latest-64.zip
+cp "${MINIMAL_IMAGE_NAME32}" latest-minimal.zip
+cp "${MINIMAL_IMAGE_NAME32}" latest-minimal-32.zip
+cp "${MINIMAL_IMAGE_NAME64}" latest-minimal-64.zip
 
-for f in Pharo7.0-*.zip; do
+for f in Pharo7.0-32bit-*.zip; do
 	#If it is not base image
-	if [[ "$f" != "FULL_IMAGE_NAME" ]]; then
+	BITNESS=32bit
+	if [[ "$f" != "FULL_IMAGE_NAME32" ]]; then
 		IMAGE_KIND=$(echo "$f" | cut -d '-' -f 2)
-		mv "$f" Pharo${IMAGE_KIND}-7.0.0-alpha.build.${BUILD_NUMBER}.sha.${HASH}.arch.32bit.zip;
+		mv "$f" Pharo${IMAGE_KIND}-7.0.0-alpha.build.${BUILD_NUMBER}.sha.${HASH}.arch.${BITNESS}.zip;
+	fi
+done
+
+for f in Pharo7.0-64bit-*.zip; do
+	#If it is not base image
+	BITNESS=64bit
+	if [[ "$f" != "FULL_IMAGE_NAME64" ]]; then
+		IMAGE_KIND=$(echo "$f" | cut -d '-' -f 2)
+		mv "$f" Pharo${IMAGE_KIND}-7.0.0-alpha.build.${BUILD_NUMBER}.sha.${HASH}.arch.${BITNESS}.zip;
 	fi
 done
