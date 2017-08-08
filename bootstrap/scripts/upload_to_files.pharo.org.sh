c35760cb59addc772f1e0b2e36d95914233da433
Guille Polito <guillermopolito@gmail.com>
2017-08-08 22:20:52 +0200

    HOTFIX: correct scp server path

--- bootstrap/scripts/upload_to_files.pharo.org.sh
+++ bootstrap/scripts/upload_to_files.pharo.org.sh
@@ -4,4 +4,4 @@ set -ex
 
 destDir="/appli/files.pharo.org/image/70/"
 echo "Uploading Images to pharo.files.org/$destDir"
-scp -o StrictHostKeyChecking=no -v *.zip files.pharo.org:$destDir
+scp -o StrictHostKeyChecking=no -v *.zip filepharosync@file-pharo.inria.fr:/appli/files.pharo.org/image/70/
