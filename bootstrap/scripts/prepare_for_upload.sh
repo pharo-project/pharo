6e7b49f99a6b1b63eea3f39e8384a98414b121a0
Guille Polito <guillermopolito@gmail.com>
2017-08-08 23:07:05 +0200

    HOTFIX: fix upload script paths

--- bootstrap/scripts/prepare_for_upload.sh
+++ bootstrap/scripts/prepare_for_upload.sh
@@ -1,3 +1,8 @@
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
 #Get the hash of the built image
 HASH=$(find Pharo7.0-32bit-*.zip | head -n 1 | cut -d '-' -f 3 | cut -d '.' -f 1)
 FULL_IMAGE_NAME32="Pharo7.0-32bit-${HASH}.zip"
@@ -14,20 +19,26 @@ cp "${MINIMAL_IMAGE_NAME32}" latest-minimal.zip
 cp "${MINIMAL_IMAGE_NAME32}" latest-minimal-32.zip
 cp "${MINIMAL_IMAGE_NAME64}" latest-minimal-64.zip
 
-for f in Pharo7.0-32bit-*.zip; do
+for f in Pharo7.0-*-32bit-*.zip; do
 	#If it is not base image
 	BITNESS=32bit
-	if [[ "$f" != "FULL_IMAGE_NAME32" ]]; then
+	if [[ "$f" != "${FULL_IMAGE_NAME32}" ]]; then
 		IMAGE_KIND=$(echo "$f" | cut -d '-' -f 2)
 		mv "$f" Pharo${IMAGE_KIND}-7.0.0-alpha.build.${BUILD_NUMBER}.sha.${HASH}.arch.${BITNESS}.zip;
+	else
+		mv "$f" Pharo-7.0.0-alpha.build.${BUILD_NUMBER}.sha.${HASH}.arch.${BITNESS}.zip;
 	fi
 done
 
-for f in Pharo7.0-64bit-*.zip; do
+for f in Pharo7.0-*-64bit-*.zip; do
 	#If it is not base image
 	BITNESS=64bit
-	if [[ "$f" != "FULL_IMAGE_NAME64" ]]; then
+	echo $f
+	echo ${FULL_IMAGE_NAME64}
+	if [[ "$f" != "${FULL_IMAGE_NAME64}" ]]; then
 		IMAGE_KIND=$(echo "$f" | cut -d '-' -f 2)
 		mv "$f" Pharo${IMAGE_KIND}-7.0.0-alpha.build.${BUILD_NUMBER}.sha.${HASH}.arch.${BITNESS}.zip;
+	else
+		mv "$f" Pharo-7.0.0-alpha.build.${BUILD_NUMBER}.sha.${HASH}.arch.${BITNESS}.zip;
 	fi
 done
