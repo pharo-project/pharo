a7cfed1f521a02e7446a4d2ce8cce246d947c90c
Pavel Krivanek <pavel.krivanek@gmail.com>
2017-12-27 18:31:36 +0100

    compress sources in memory

--- bootstrap/scripts/build.sh
+++ bootstrap/scripts/build.sh
@@ -199,6 +199,7 @@ dd if="displaySize.bin" of="${PHARO_IMAGE_NAME}.image" bs=1 seek=$SEEK count=4 c
 ${VM} "${PHARO_IMAGE_NAME}.image" eval --save "Metacello new baseline: 'Tonel';repository: 'github://pharo-vcs/tonel:v1.0.5'; load: 'core'"
 ${VM} "${PHARO_IMAGE_NAME}.image" eval --save "Metacello new baseline: 'IDE';repository: 'tonel://${REPOSITORY}/src'; load"
 ${VM} "${PHARO_IMAGE_NAME}.image" eval --save "FFIMethodRegistry resetAll. PharoSourcesCondenser condenseNewSources"
+${VM} "${PHARO_IMAGE_NAME}.image" eval --save "CompressedSegmentsStream compressSourcesInMemory"
 ${VM} "${PHARO_IMAGE_NAME}.image" clean --release
 
 echo "[Pharo] Configure resulting image"
