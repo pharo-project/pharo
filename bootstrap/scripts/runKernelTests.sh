5bf593cd10aa7ea0edf944d20cc1668302e60cc6
Pablo Tesone <tesonep@gmail.com>
2018-02-14 14:24:04 +0100

    Fixing the creation of the Kernel Test image

--- bootstrap/scripts/runKernelTests.sh
+++ bootstrap/scripts/runKernelTests.sh
@@ -31,8 +31,17 @@ mv $IMAGE_FILE bootstrap.image
 
 export PHARO_CI_TESTING_ENVIRONMENT=1
 			
+#Initializing the Image
 ./pharo bootstrap.image
-./pharo bootstrap.image initializePackages --packages=packagesKernel.txt --protocols=protocolsKernel.txt --save
+#Adding packages removed from the bootstrap
 ./pharo bootstrap.image loadHermes Hermes-Extensions.hermes --save
+./pharo bootstrap.image loadHermes Collections-Atomic.hermes AST-Core.hermes Collections-Arithmetic.hermes 
+
+#Initializing the package manager
+./pharo bootstrap.image initializePackages --packages=packagesKernel.txt --protocols=protocolsKernel.txt --save
+
+#Loading Tests
 ./pharo bootstrap.image loadHermes SUnit-Core.hermes JenkinsTools-Core.hermes JenkinsTools-Core.hermes SUnit-Tests.hermes --save --no-fail-on-undeclared --on-duplication=ignore
+
+#Running tests.
 ./pharo bootstrap.image test --junit-xml-output SUnit-Core SUnit-Tests	
