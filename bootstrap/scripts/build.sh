840aa19d622b7dfe456e9477fbff5bd3967c1869
Pablo Tesone <tesonep@gmail.com>
2018-03-08 16:20:21 +0100

    Moving System-Model to Monticello. Test Suite is not extending anymore Model, as it was not using it

--- bootstrap/scripts/build.sh
+++ bootstrap/scripts/build.sh
@@ -150,7 +150,7 @@ echo "[Compiler] Adding more Kernel packages"
 ${VM} "${COMPILER_IMAGE_NAME}.image" loadHermes Hermes-Extensions.hermes --save
 ${VM} "${COMPILER_IMAGE_NAME}.image" loadHermes Multilingual-Encodings.hermes Multilingual-TextConversion.hermes Multilingual-Languages.hermes --save --no-fail-on-undeclared
 
-${VM} "${COMPILER_IMAGE_NAME}.image" loadHermes InitializePackagesCommandLineHandler.hermes System-Model.hermes --save
+${VM} "${COMPILER_IMAGE_NAME}.image" loadHermes InitializePackagesCommandLineHandler.hermes --save
 
 ${VM} "${COMPILER_IMAGE_NAME}.image" loadHermes Collections-Atomic.hermes AST-Core.hermes Collections-Arithmetic.hermes Jobs.hermes --save --no-fail-on-undeclared
 
