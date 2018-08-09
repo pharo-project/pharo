b20611f06d7e3222db92d3c03afc0b2a9314ac7b
AlistairGrant <akgrant0710@gmail.com>
2018-08-09 09:53:05 +0200

    22304: Update bootstrap/scripts/snapshot.sh
    to correctly reference the bootstrap cache directory.

--- bootstrap/scripts/snapshot.sh
+++ bootstrap/scripts/snapshot.sh
@@ -12,5 +12,5 @@ set -e
 . ${BOOTSTRAP_REPOSITORY:-.}/bootstrap/scripts/envvars.sh
 
 pushd "${BOOTSTRAP_REPOSITORY}"
-tar czf "${BOOTSTRAP_REPOSITORY}/snapshot.tar.gz" Pharo.image Pharo.changes pharo pharo-ui pharo-vm pharo-local vmtarget bootstrap-cache
+tar czf "${BOOTSTRAP_REPOSITORY}/snapshot.tar.gz" Pharo.image Pharo.changes pharo pharo-ui pharo-vm pharo-local ${CACHE}
 popd
