6f2efc8a44b134ace9b8a86c51afdaa228c12fb2
Guille Polito <guillermopolito@gmail.com>
2018-09-20 09:39:56 +0200

    Add version info in build script

--- bootstrap/scripts/3-prepare.sh
+++ bootstrap/scripts/3-prepare.sh
@@ -10,5 +10,5 @@ SCRIPTS="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)"
 . ${SCRIPTS}/envvars.sh
 
 ./pharo Pharo.image ${BOOTSTRAP_REPOSITORY}/bootstrap/scripts/prepare_image.st --save --quit
-./pharo Pharo.image ${BOOTSTRAP_REPOSITORY}/bootstrap/scripts/bootstrap.st --ARCH=${BOOTSTRAP_ARCH} --BUILD_NUMBER=${BUILD_NUMBER} --quit
+./pharo Pharo.image ${BOOTSTRAP_REPOSITORY}/bootstrap/scripts/bootstrap.st --ARCH=${BOOTSTRAP_ARCH} --BUILD_NUMBER=${BUILD_NUMBER} --VERSION_INFO=`git describe --long --tags --abbrev=40` --quit
 
