081a8431a148c819777fd63bf99f1678c729a958
Esteban Lorenzano <estebanlm@gmail.com>
2018-11-13 11:13:46 +0100

    do not use git to determine vm version on tests, because I do not have git here :P

--- bootstrap/scripts/runTests.sh
+++ bootstrap/scripts/runTests.sh
@@ -6,9 +6,6 @@ set -o pipefail
 set -o nounset
 set -o xtrace
 
-SCRIPTS="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)"
-. ${SCRIPTS}/envversion.sh
-
 # The first parameter is the architecture
 # The second parameter is the stage name
 
@@ -16,12 +13,16 @@ CACHE="${BOOTSTRAP_CACHE:-bootstrap-cache}"
 
 find ${CACHE}
 
+# I will use the name of the image to determine the vm version (because file name is in the format Pharo7.0.0-rc1)
+IMAGE_ARCHIVE=$(find ${CACHE} -name Pharo*-${1}bit-*.zip)
+# take VM version
+PHARO_VM_VERSION=$(echo "${IMAGE_ARCHIVE}" | cut -d'-' -f 1 | cut - c 6- | cut -d'.' -f 1-2 | sed 's/\.//')
+# now download vm
 ${BOOTSTRAP_REPOSITORY:-.}/bootstrap/scripts/getPharoVM.sh ${PHARO_VM_VERSION} vm ${1}
-					
-IMAGE_ARCHIVE=$(find ${CACHE} -name ${PHARO_NAME_PREFIX}-${1}bit-*.zip)
+# ...and continue with the processing
 unzip $IMAGE_ARCHIVE
-IMAGE_FILE=$(find . -name ${PHARO_NAME_PREFIX}-${1}bit-*.image)
-CHANGES_FILE=$(find . -name ${PHARO_NAME_PREFIX}-${1}bit-*.changes)
+IMAGE_FILE=$(find . -name Pharo*-${1}bit-*.image)
+CHANGES_FILE=$(find . -name Pharo*-${1}bit-*.changes)
 				
 cp ${CACHE}/*.sources .
 mv $IMAGE_FILE Pharo.image
