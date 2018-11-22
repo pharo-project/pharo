94efebb7789531ab282aa2812c0888e2d25cc2f7
Esteban Lorenzano <estebanlm@gmail.com>
2018-11-22 16:40:42 +0100

    using $() instead backsticks

--- bootstrap/scripts/envversion.sh
+++ bootstrap/scripts/envversion.sh
@@ -6,11 +6,11 @@
 # - vm version for the current image, to be used to download the correct vm, e.g. "70"
 
 # Versions are taken from tags. Tag should have a format like vMAJOR.MINOR.PATCH[-EXTRA], e.g. v7.0.0-rc1
-PHARO_VERSION_PREFIX="Pharo`git describe --long --tags | cut -d'-' -f 1-2 | cut -c 2-`"
+PHARO_VERSION_PREFIX="Pharo$(git describe --long --tags | cut -d'-' -f 1-2 | cut -c 2-)"
 # Snapshots are taken from branchs. Branchs should be named dev-MAJOR.MINOR (example: dev-7.0)
-PHARO_SNAPSHOT_PREFIX="Pharo`git branch | grep \* | cut -d' ' -f 2 | cut -d'-' -f 2`-SNAPSHOT"
+PHARO_SNAPSHOT_PREFIX="Pharo$(git branch | grep \* | cut -d' ' -f 2 | cut -d'-' -f 2)-SNAPSHOT"
 
-PHARO_SHORT_VERSION=`git branch | grep \* | cut -d' ' -f 2 | cut -d'-' -f 2 | sed 's/\.//'`
+PHARO_SHORT_VERSION=$(git branch | grep \* | cut -d' ' -f 2 | cut -d'-' -f 2 | sed 's/\.//')
 # this is just to make things clear (and because they could change in the future, who knows :P)
 PHARO_VM_VERSION="${PHARO_SHORT_VERSION}"
 
