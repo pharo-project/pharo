88c223a200f95ebdcbaa81d678928d71c1f14ee5
Guille Polito <guillermopolito@gmail.com>
2023-12-04 16:17:51 +0100

    Load Random-Core for kernel tests

--- bootstrap/scripts/runKernelTests.sh
+++ bootstrap/scripts/runKernelTests.sh
@@ -57,7 +57,7 @@ export PHARO_CI_TESTING_ENVIRONMENT=1
 ./pharo bootstrap.image
 #Adding packages removed from the bootstrap
 ./pharo bootstrap.image loadHermes Hermes-Extensions.hermes --save
-./pharo bootstrap.image loadHermes  Kernel-Chronology-Extras.hermes AST-Core.hermes Jobs.hermes InitializePackagesCommandLineHandler.hermes --save --no-fail-on-undeclared --on-duplication=ignore
+./pharo bootstrap.image loadHermes  Kernel-Chronology-Extras.hermes AST-Core.hermes Jobs.hermes InitializePackagesCommandLineHandler.hermes Random-Core.hermes --save --no-fail-on-undeclared --on-duplication=ignore
 
 #Initializing the package manager
 ./pharo bootstrap.image initializePackages --packages --protocols=protocolsKernel.txt --save
