235a91622e80b979e748e77632344dd43ce92ef1
CyrilFerlicot <cyril@ferlicot.fr>
2025-09-25 14:23:31 +0200

    Cleaning of DateAndTime
    DateAndTime class>>clock has no setter so its value cannot be changed.
    Most of the users of this methods just forward things to Time to call a method of the same name.
    
    I propose to deprecate #clock and use Time instead. Also, there is a moment in the bootstrap when the clock is not initialized and causes crashes. This remove the need for the initialization

--- bootstrap/scripts/runKernelTests.sh
+++ bootstrap/scripts/runKernelTests.sh
@@ -64,7 +64,6 @@ export PHARO_CI_TESTING_ENVIRONMENT=1
 ./pharo bootstrap.image perform --save Pragma buildCache
 ./pharo bootstrap.image loadHermes System-Time.hermes AST-Core.hermes Random-Core.hermes System-NumberPrinting.hermes --save --no-fail-on-undeclared --on-duplication ignore
 ./pharo bootstrap.image perform --save ChronologyConstants initialize
-./pharo bootstrap.image perform --save DateAndTime initialize
 
 #Initializing the package manager
 ./pharo bootstrap.image perform --save PharoBootstrapFixMethodsTool fixExtensionMethods
