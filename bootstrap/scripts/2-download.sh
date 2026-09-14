3b8750a8ef39adbb5545bc58777644e186462902
guillep <guillermopolito@gmail.com>
2026-09-14 11:35:05 +0200

    Use version bootstrap 2.0.0

--- bootstrap/scripts/2-download.sh
+++ bootstrap/scripts/2-download.sh
@@ -51,7 +51,7 @@ if [ ! -e "${BOOTSTRAP_DOWNLOADS}/vmBootstrap/pharo" ]; then
 fi 
 
 if [ ! -e "${BOOTSTRAP_DOWNLOADS}/bootstrapImage.zip" ]; then
-	download_to https://github.com/guillep/PharoBootstrap/releases/download/v1.7.10/bootstrapImage.zip ${BOOTSTRAP_DOWNLOADS}/bootstrapImage.zip
+	download_to https://github.com/pharo-project/PharoBootstrap/releases/download/v2.0.0/bootstrapImage.zip ${BOOTSTRAP_DOWNLOADS}/bootstrapImage.zip
 fi 
 
 # checking for icons
