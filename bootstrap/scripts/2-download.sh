252b0162f4d72b883724a3f65758a47cab5f168b
Pablo Tesone <tesonep@gmail.com>
2026-09-22 13:51:32 +0200

    Updating VM to fix build

--- bootstrap/scripts/2-download.sh
+++ bootstrap/scripts/2-download.sh
@@ -33,7 +33,7 @@ if [ ! -e "${BOOTSTRAP_VMTARGET}" ]; then
 
 	#Odd PR builds use the the latest VM, else use the stable VM
 	set_version_variables
-	TEST_VM_KIND="vmLatest"
+	TEST_VM_KIND="vm"
 	${BOOTSTRAP_REPOSITORY}/bootstrap/scripts/getPharoVM.sh ${PHARO_SHORT_VERSION} ${TEST_VM_KIND} $BOOTSTRAP_ARCH
 
   cd -
